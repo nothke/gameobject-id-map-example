29abad1a2a59ba6845c8686f26a7895f87a534e5
Ivan Notaros <nothke@gmail.com>
2022-06-28 05:37:22 +0200

    Added annotations

--- src/main.cpp
+++ src/main.cpp
@@ -3,30 +3,30 @@
 #include <vector>
 #include <map>
 
-struct Model {
-	bool alive{ false };
-	int a{ 0 };
-	int b{ 0 };
-};
-
-struct Scene;
-
 using u32 = unsigned int;
 
-// Trivially copiable
-struct GameObject {
-	Scene& scene;
-	u32 id{ 0 }; // maps into the map
-
-	Model& Get();
+// This is our actual object that holds some data
+struct Model {
+	int a{ 0 }, b{ 0 };
+	float x{ 0 }, y{ 0 }, z{ 0 };
 };
 
+struct Scene; // just a forward declaration necessary for GameObject
 
+// A trivially copiable wrapper to the Model
+struct GameObject {
+	Scene& scene;	// reference to the scene
+	u32 id{ 0 };	// maps into the map
+
+	Model& Get(); // just forward declared here necessary for Scene (implemented below Scene)
+};
+
+// The object that holds our model and map
 struct Scene {
-	std::vector<Model> models;
-	std::map<u32, u32> map; // id to models vector index
+	std::vector<Model> models; // this is our actual contiguous array of data
+	std::map<u32, u32> map; // a dictionary that maps ids to models vector index
 
-	u32 last{ 0 };
+	u32 last{ 0 }; // incremented to always provide a unique id for new objects
 
 	static constexpr u32 NULL_ID{ 0 }; // a 0 should map to invalid (the gameobject is removed)
 
@@ -34,14 +34,15 @@ struct Scene {
 		models.reserve(12);
 	}
 
+	// Used only by the GameObject
 	Model& __GetModelById(u32 id) {
 		assert(map.at(id) != NULL_ID);
 		return models[map.at(id) - 1];
 	}
 
+	// Creates a new GameObject in Scene
 	GameObject Create() {
 		auto& model = models.emplace_back();
-		model.alive = true;
 
 		// the new object will always be last
 		u32 newIndex = static_cast<u32>(models.size());
@@ -50,6 +51,7 @@ struct Scene {
 		return GameObject{ *this, last++ };
 	}
 
+	// Removes a GameObject from this Scene
 	void Destroy(const GameObject& go) {
 		u32 mi = map.at(go.id) - 1; // model index
 		models.erase(models.begin() + mi);
@@ -97,6 +99,8 @@ void DebugScene(Scene& scene)
 int main(int argc, char* argv[]) {
 	Scene scene;
 
+	// Create 3 objects
+
 	auto go = scene.Create();
 
 	go.Get().a = 10;
@@ -110,10 +114,12 @@ int main(int argc, char* argv[]) {
 
 	DebugScene(scene);
 
+	// Destroy one object
 	scene.Destroy(go2);
 
 	DebugScene(scene);
 
+	// Creating some more objects
 	for (size_t i = 0; i < 5; i++)
 	{
 		auto _go = scene.Create();
@@ -122,6 +128,7 @@ int main(int argc, char* argv[]) {
 
 	DebugScene(scene);
 
+	// Removing objects
 	for (size_t i = 3; i < 6; i++)
 	{
 		GameObject _go = scene.GetById(i);
@@ -130,10 +137,18 @@ int main(int argc, char* argv[]) {
 
 	DebugScene(scene);
 
-	for (size_t i = 0; i < 20; i++)
+	// Creating many more objects
+	for (size_t i = 0; i < 200; i++)
 	{
 		auto _go = scene.Create();
 		_go.Get().a = 200 + i;
+	}
+
+	// Removing many objects
+	for (size_t i = 50; i < 150; i++)
+	{
+		auto _go = scene.GetById(i);
+		scene.Destroy(_go);
 	}
 
 	DebugScene(scene);
